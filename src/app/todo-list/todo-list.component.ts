70e4e2a134ee1024f137958ce9f26b1c4e0e4ee1
Xinwen Xiang <XXiang2@slb.com>
2023-11-10 14:15:45 +0800

    feat: finish markDone Service

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from "@angular/core";
-import { ToDoItem } from "src/model/ToDoItem";
-import { TodoService } from "../service/todo.service";
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+import { TodoService } from '../service/todo.service';
 
 @Component({
-  selector: "app-todo-list",
-  templateUrl: "./todo-list.component.html",
-  styleUrls: ["./todo-list.component.css"],
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   constructor(private todoService: TodoService) {}
   todoList: ToDoItem[] = [];
-
   ngOnInit() {
     this.todoList = this.todoService.getAll();
+  }
+
+  onMarkDone(id: number) {
+    this.todoService.markDone(id);
   }
 }
