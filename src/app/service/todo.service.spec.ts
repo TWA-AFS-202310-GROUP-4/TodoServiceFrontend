0ac20e63d1e10ebebbceefc3034d594a84595792
Ming Wang <MWang35@slb.com>
2023-11-10 14:21:09 +0800

    feat: add service getAll test

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,22 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id : 1,
+        title : 'hh',
+        description : 'hh',
+        isDone : true
+      }
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll', () => {
+    service.getAll();
+    expect(service.items).toEqual(service.items);
   });
 });
