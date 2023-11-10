01a0ec3951ff5238ea30cb6880f88f3c9abfe310
Xinwei <xlu15@slb.com>
2023-11-10 11:30:10 +0800

    feat: todolist component with mock data

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  items: ToDoItem [] = [ {
+    id: 1,
+    title: 'milk',
+    description: "Buy milk",
+    isDone: false
+  }, 
+  {
+    id: 2,
+    title: 'milk',
+    description: "Buy milk",
+    isDone: false
+  }]
+}
