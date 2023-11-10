c62f9550d8d557b71c9e661eac5d3f356ab46f8f
Xinwei <xlu15@slb.com>
2023-11-10 15:05:18 +0800

    feat: get item detail from todo-detail component

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToDoItem } from 'src/model/ToDoItem';
+import { TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.css']
 })
-export class TodoDetailComponent {
 
+export class TodoDetailComponent {
+  item: ToDoItem | undefined
+
+  constructor(
+    private activatedRouter:ActivatedRoute,
+    private todoService: TodoService) {}
+
+  ngOnInit() {
+    const id = this.activatedRouter.snapshot.paramMap.get('detailId')
+    console.log(id)
+    this.item = this.todoService.getItemById(Number(id))
+  }
 }
