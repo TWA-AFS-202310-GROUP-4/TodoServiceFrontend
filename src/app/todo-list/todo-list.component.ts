139488b59aa5756de2b619f0bbf6326ced81e6a9
Jingbo Yu <Jyu20@slb.com>
2023-11-10 13:10:00 +0800

    feat: create createTodo component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,21 +4,21 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   items: ToDoItem[] = [
     {
       id: 0,
-      title: "t1",
-      description: "d1",
-      isDone: false
+      title: 't1',
+      description: 'd1',
+      isDone: false,
     },
     {
       id: 1,
-      title: "t2",
-      description: "d2",
-      isDone: false
-    }
-  ]
+      title: 't2',
+      description: 'd2',
+      isDone: false,
+    },
+  ];
 }
