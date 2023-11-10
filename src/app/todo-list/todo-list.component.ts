9daf9f982bf9feb55b193cf19c69458849ba639d
Diana Jing <DJing@slb.com>
2023-11-10 13:06:05 +0800

    refactor: reformat by using prettier

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,17 +4,11 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   items: ToDoItem[] = [
-    {id:1,
-    title:"buy milk",
-    description:"like",
-    isDone:false},
-    {id:2,
-      title:"buy bread",
-      description:"like",
-      isDone:false}
-  ]
+    { id: 1, title: 'buy milk', description: 'like', isDone: false },
+    { id: 2, title: 'buy bread', description: 'like', isDone: false },
+  ];
 }
