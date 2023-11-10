739f66ed6a6ad8c8c1db43b4abad26758d18daeb
Xinwei <xlu15@slb.com>
2023-11-10 14:20:51 +0800

    test: service get all test complete

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,25 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [{
+      id: 1,
+      title: 'milk',
+      description: "Buy milk",
+      isDone: false
+    }]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should get all items when call getAll", () => {
+    const items = service.getAll()
+    expect(items).toEqual([{
+        id: 1,
+        title: 'milk',
+        description: "Buy milk",
+        isDone: false
+      }])
+  })
 });
