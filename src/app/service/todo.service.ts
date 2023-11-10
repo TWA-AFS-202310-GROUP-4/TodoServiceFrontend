18f240f051dbfbe2a478447bd8113ca9968ee49e
Diana Jing <DJing@slb.com>
2023-11-10 13:47:22 +0800

    feat: add create method

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -2,18 +2,25 @@ import { Injectable } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
-
   items: ToDoItem[] = [
     { id: 1, title: 'buy milk', description: 'like', isDone: false },
     { id: 2, title: 'buy bread', description: 'like', isDone: false },
   ];
-  constructor() { }
+  constructor() {}
 
-  getAll(){
-    return this.items
+  getAll() {
+    return this.items;
   }
 
+  create(title: string, description: string) {
+    this.items.push({
+      id: this.items.length + 1,
+      title: title,
+      description: description,
+      isDone: false,
+    });
+  }
 }
