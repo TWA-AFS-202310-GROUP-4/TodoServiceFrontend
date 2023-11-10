2e55c30b3e9d84315be2b5b969bfdd3b86067a3d
Dexin Wang <DWang36@slb.com>
2023-11-10 11:30:10 +0800

    feat: add todolist component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+
+  items: ToDoItem[] = [
+    {
+    id: 1,
+    title: "sss",
+    description:"sss",
+    isDone: false
+  },
+  {
+    id: 2,
+    title: "sss",
+    description:"sss",
+    isDone: false
+  },
+]
+
+
+}
