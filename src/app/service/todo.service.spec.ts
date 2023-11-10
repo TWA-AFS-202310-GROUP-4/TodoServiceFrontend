cfd642cd184f29a888b7246e10f7a4a88a126d56
Diana Jing <DJing@slb.com>
2023-11-10 14:29:06 +0800

    test: add test cases for service

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,41 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'yummy',
+        isDone: false,
+      },
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all items when call getAll', () => {
+    const item = service.getAll();
+    expect(item).toEqual([
+      { id: 1, title: 'buy milk', description: 'yummy', isDone: false },
+    ]);
+  });
+
+  it('should create a new item when call create', () => {
+
+    service.create('buy','good');
+    expect(service.items).toEqual([
+      { id: 1, title: 'buy milk', description: 'yummy', isDone: false },
+      { id: 2, title: 'buy', description: 'good', isDone: false },
+    ]);
+  });
+
+  it('should mark isDone true when call markdone', () => {
+
+    service. markDone(1);
+    expect(service.items[0].isDone).toEqual(true);
+  });
+
+
 });
