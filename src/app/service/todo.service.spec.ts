228329ad69fe8ca645efe96a7bc689d09ae91d5b
Dexin Wang <DWang36@slb.com>
2023-11-10 14:25:38 +0800

    test:add the creat and markdone test

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -30,4 +30,34 @@ describe('TodoService', () => {
         isDone: false}
     ])
   });
+
+  
+  it('should make isDone = true when call markdown', () => {
+
+    service.markdown(1);
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: "xianke",
+        description: "xianke",
+        isDone: true}
+    ])
+  });
+
+    it('should add a item when call creat', () => {
+
+    service.creat("xianke2","xianke2")
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: "xianke",
+        description: "xianke",
+        isDone: false},
+        {
+          id: 2,
+          title: "xianke2",
+          description: "xianke2",
+          isDone: false}
+    ])
+  });
 });
