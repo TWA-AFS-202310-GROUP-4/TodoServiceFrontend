3e78a75a17f2c28088430ea27debf5650eb2fb75
Ming Wang <MWang35@slb.com>
2023-11-13 00:31:04 +0800

    feat: add http service test

--- src/app/service/todo-http.service.spec.ts
+++ src/app/service/todo-http.service.spec.ts
@@ -11,8 +11,23 @@ describe('TodoHttpService', () => {
   let service: TodoHttpService;
   let httpClientSpy : jasmine.SpyObj<HttpClient>;
 
+  const postItem = {
+    id: 1,
+    title: "buy milk",
+    description : "buy a bottle of milk",
+    isDone: false
+  }
+  const expectedResponse = {
+    id: 1,
+    title: "buy milk",
+    description : "buy a bottle of milk",
+    isDone: false
+  }
+
+  const mockId = 1;
+
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get','post','put','delete'])
     service = new TodoHttpService(httpClientSpy);
   });
 
@@ -38,5 +53,39 @@ describe('TodoHttpService', () => {
     );
 
     expect(httpClientSpy.get.calls.count()).toEqual(1);
+  });
+
+  it('should create a todo item and return the created when call create', () => {
+    const url = 'https://localhost:44309/ToDoItem'
+    httpClientSpy.post.and.returnValue(asyncData(postItem))
+    service.create(postItem).subscribe(res => {
+      expect(res).toEqual(expectedResponse)
+    })
+    expect(httpClientSpy.post).toHaveBeenCalledWith(url, postItem)
+  });
+
+  it('should return item by ID when call getById given id', () => {
+    httpClientSpy.get.and.returnValue(asyncData(expectedResponse))
+    service.getById(mockId).subscribe(res => {
+      expect(res).toEqual(expectedResponse)
+    })
+    expect(httpClientSpy.get.calls.count()).toEqual(1)
+  });
+
+  it('should return deleted item when call delete given id', () => {
+    httpClientSpy.delete.and.returnValue(asyncData(expectedResponse))
+    service.delete(mockId).subscribe(res => {
+      expect(res).toEqual(expectedResponse)
+    })
+    expect(httpClientSpy.delete).toHaveBeenCalled()
+  });
+
+  it('should return the updated item when call update given id and new item', () => {
+    const url = `https://localhost:44309/ToDoItem/${mockId}`
+    httpClientSpy.put.and.returnValue(asyncData(postItem))
+    service.update(mockId, postItem).subscribe(res => {
+      expect(res).toEqual(expectedResponse)
+    })
+    expect(httpClientSpy.put).toHaveBeenCalledWith(url, postItem)
   });
 });
